8fd2ccd8696fc84a3a3bf03559955e8d5623b508
Mykola Radionov <apelsinbanan07@gmail.com>
2026-02-21 18:18:52 +0200

    Fixed analyzer logic and scoring

--- analyzer.py
+++ analyzer.py
@@ -11,34 +11,47 @@ def scan_emails():
     config = load_config()
     rules = config.get('rules', [])
     thresholds = config.get('thresholds', {"critical": 70, "warning": 25})
+    
     if not os.path.exists('emails'):
-        print(" Folder 'emails' not found! Run generator.py first.")
+        print("Folder 'emails' not found!")
         return
+
     email_files = os.listdir('emails')
     results_for_table = []
+
+    
     for filename in email_files:
         path = os.path.join('emails', filename)
+        
+       
         with open(path, 'r', encoding='utf-8') as f:
             content = f.read()
-            total_score = 0
-            detected = []
+        
+      
+        total_score = 0
+        detected = []
+        
+        print(f"🔍 DEBUG: Checking {filename}...") 
 
-            for rule in rules:
-                if re.search(rule['pattern'], content):
-                    total_score += rule['points']
-                    detected.append(rule['id'])
+      
+        for rule in rules:
+            if re.search(rule['pattern'], content):
+                print(f"   !!! MATCH FOUND: {rule['id']} (Score: +{rule['points']})")
+                total_score += rule['points']
+                detected.append(rule['id'])
 
-            
-            if total_score >= thresholds.get('critical', 70):
-                status = " CRITICAL"
-            elif total_score >= thresholds.get('warning', 25):
-                status = " WARNING"
-            else:
-                status = "CLEAN"
+      
+        if total_score >= thresholds.get('critical', 70):
+            status = " CRITICAL"
+        elif total_score >= thresholds.get('warning', 25):
+            status = " WARNING"
+        else:
+            status = "CLEAN"
 
-            results_for_table.append([filename, total_score, status, ", ".join(detected)])
+        results_for_table.append([filename, total_score, status, ", ".join(detected)])
 
-    print(tabulate(results_for_table, headers=["File", "Score", "Status", "Detected"], tablefmt="fancy_grid"))
+
+    print("\n" + tabulate(results_for_table, headers=["File", "Score", "Status", "Detected"], tablefmt="fancy_grid"))
 
 if __name__ == "__main__":
     scan_emails()
